3dcfe5f267f6f7efa451b3acdde9882473ed6e06
anchor <anchor@host-155-246-141-14.dhcp.stevens-tech.edu>
2017-12-09 23:06:48 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//
+hbs.registerPartials(__dirname + '/views/partials')
+
+//set have (set key, value use)
+app.set('view engine', 'hbs');
+// middleware, __dirname us use to path, but if want to have privacy, need to change to next app.use function
+
+
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+
+});
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+app.get('/',(req,res) => {
+    //res.send('<h1>Hello express!</h1>');
+    /*res.send({
+        name: 'Andrew',
+        likes: [
+            'Biking',
+            'Cities'
+        ]
+    })*/
+    res.render('home.hbs',{
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+    })
+
+});
+
+app.get('/about', (req,res) => {
+    //render the template
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000');
+});
